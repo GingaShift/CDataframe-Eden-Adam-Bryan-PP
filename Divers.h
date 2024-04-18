360894c17c8ca7aa4bf399044f0f681e4af6b6d2
delfassy <elfdav@yahoo.fr>
2024-04-18 20:43:04 +0200

    Reso du bug acces dossier debug protege

--- Divers.h
+++ Divers.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <stdio.h>
 
+// Définition d'une énumération pour représenter un type booléen
+typedef enum { false, true } bool;
+
 void free_ressources(COLONNE** dataframe, int taille_dataframe);
--- main.c
+++ main.c
@@ -41,54 +41,68 @@ int modifier_valeur(COLONNE** dataframe, int taille_dataframe, int num_col, int 
 int main()
 {
     // Creation et ini du CDataframe
-    int taille_CDataframe;
-    COLONNE** CDataframe;
+    int taille_CDataframe = -1;
+    COLONNE** CDataframe = NULL;
+    bool CDataframe_exists = false;
 
     printf("************************************************************************\n");
     printf("*                                                                      *\n");
     printf("*                      =-    MENU GENERAL    -=                        *\n");    
     printf("*                                                                      *\n");
     printf("************************************************************************\n");
-
+    printf("\n");
+    printf("1. Creer le CDataframe\n\n");
+    printf("2. Charger un CDataframe\n\n");
+    printf("3. Creer une colonne\n\n");
+    printf("0. Quitter le programme\n\n");
+    
     int choix = 0;
     int go = 1;
 
     // Boucle jusqu'à ce que la saisie soit valide
     while (go == 1)
     {
-        printf("\n\n");
-        printf("1. Creer CDataframe (tapez 1)\n\n");
-        printf("2. Charger CDataframe (tapez 2)\n");
-
+        printf("\nChoisissez un des numeros puis validez en appuyant sur la touche Entree\n\n");
         scanf("%d", &choix);
 
-        /*if (choix == 1)
+        if (choix == 1)
         {
-            CDataframe = creer_cdataframe(&taille_CDataframe);
+            if (CDataframe_exists == false)
+            {
+                CDataframe = creer_cdataframe(&taille_CDataframe);
+                if (CDataframe == NULL && taille_CDataframe == 0)
+                {
+                    printf("Le CDataframe a ete cree avec succes\n");
+                    CDataframe_exists = true;
+                    
+                    // Peupler le CDataframe artificiellement - Test seulement
+                    populate_dataframe_artificially(&CDataframe, &taille_CDataframe);
+
+
+                }
+            }
+            else
+                printf("Le CDataframe a deja ete cree\n");
         }
         else if (choix == 2)
-            printf("Le chargement d'un CDataframe sera bientot disponible\n");*/
+            printf("Le chargement d'un CDataframe sera bientot disponible...\n");
 
-        
-        if (choix==1)
+        if (choix == 0)
             go = 0;
     }
-     
-
-
+    
     // Libération de la mémoire occupée par les colonnes et le dataframe
     free_ressources(CDataframe, taille_CDataframe);
 
     return 0;
+}
 
-// Peupler le CDataframe artificiellement - Test seulement
-//populate_dataframe_artificially(&CDataframe, &taille_CDataframe);
 
 //OPERATEURS_DE_COMPARAISON egal = { EGAL };
 //OPERATEURS_DE_COMPARAISON superieur = { SUPERIEUR };
 //OPERATEURS_DE_COMPARAISON inferieur = { INFERIEUR };
 //stats_sur_valeur(CDataframe, taille_CDataframe, 10, egal);
 
-}
+
 
 
